090a4bc90014ad76bf89e88c1cf19226981934c3
Henrique Marques <uchihcl@gmail.com>
2024-09-09 12:57:47 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal (São Paulo, 6 de setembro de 2008), também conhecida como Fadinha, é uma skatista brasileira, campeã olímpica e uma das mais jovens medalhistas olímpicas da história.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate, fada, sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza (Rio de Janeiro, 15 de março de 2001) é uma judoca brasileira, medalhista mundial júnior. É considerada uma das grandes promessas do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô, judoca"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade (Belo Horizonte, 12 de abril de 1999) é uma ginasta brasileira, medalhista olímpica e mundial. Conquistou a primeira medalha de ouro para o Brasil na ginástica artística feminina nos Jogos Olímpicos de Tóquio 2020.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "medalha, ouro, ginastica"
+    }
+
+];
+
